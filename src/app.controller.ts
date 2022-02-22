61a10dd91255b38d67a7bc71962f388087fee173
Diego Guerra <diegoguerraorellana146@gmail.com>
2022-02-22 20:58:32 -0300

    Prueba técnica RindeGastos

---
+++ src/app.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get,Param } from '@nestjs/common';
+import { concat } from 'rxjs';
+import { AppService } from './app.service';
+import axios from 'axios';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+
+  /*@Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }/*
+
+  /* https://localhost:3000/getConvertedAmount/CLP/USD/15000 */
+
+  @Get('/getConvertedAmount/:from/:to/:amount')
+  async getConvertedAmount(@Param('from') from: string, @Param('to') to: string, @Param('amount') amount: string) {
+    return await this.appService.getConvertedAmount(from, to, amount);
+  }
+
+  /* https://localhost:3000/getDaysUntilMyBirthday/15-03-2021 */
+
+  @Get('/getDaysUntilMyBirthday/:birthdate')
+  getDaysUntilMyBirthday(@Param('birthdate') birthdate: string) {  
+    return this.appService.getDaysUntilMyBirthday(birthdate);
+  }
+
+  /* https://localhost:3000/getTheNumber/192/3 */
+
+  @Get('/getTheNumber/:first/:second')
+  getTheNumber(@Param('first') first: number, @Param('second') second: number) {
+    return this.appService.getTheNumber(first, second);
+  }
+
+}
---
+++ src/app.service.ts
@@ -0,0 +1,73 @@
+import { Injectable,Param } from '@nestjs/common';
+import axios from 'axios';
+import { json } from 'stream/consumers';
+
+@Injectable()
+export class AppService {
+  /*getHello(): string {
+    return 'Hello World!';
+  }*/
+
+  // EJERCICIO 1
+  async getConvertedAmount(from, to, amount) {
+    try {
+      const URL = `https://free.currconv.com/api/v7/convert?q=${from}_${to},${to}_${from}&compact=ultra&apiKey=cacb99669510e1d11e20`;
+      const response = await axios.get(URL);
+      const d = response.data;
+      const exchangeTo = d[`${to}_${from}`];
+      const convertedAmount = (amount/exchangeTo).toFixed(2);
+      const objResponse = JSON.stringify({ from, to, convertedAmount });
+      return objResponse;    
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // EJERCICIO 2
+  getDaysUntilMyBirthday(birthdate) {
+    // Trabajar input fecha
+    let f = birthdate.split('-');
+    let date = new Date(f[2], f[1], f[0]);
+
+    let day = date.getUTCDate();
+    let month = date.getUTCMonth();
+    let year = date.getUTCFullYear();
+
+    // Calcular fecha cumpleaños
+    let today = new Date();
+    let myBirthday = new Date(today.getUTCFullYear(), month - 1, day);
+
+    // Verificar si cumpleaños ya pasó
+    if (today.getTime() > myBirthday.getTime()) {
+      myBirthday.setFullYear(myBirthday.getFullYear()+1);
+    }
+  
+    // Calcular diferencia de tiempo
+    let diff = myBirthday.getTime() - today.getTime();
+
+    // Transformar segundos a días
+    let days = Math.floor(diff/(1000*60*60*24));
+
+    return days;
+  }
+
+  // EJERCICIO 3
+  getTheNumber(first, second) {
+    let final: string = '';
+    for (let i = 1;i <= second;i++) {
+      final = final + (first*i);
+    }
+    
+    let largo = final.length;
+  
+    // Se evalua si el largo del string es mayor a 9, si lo es el string es cortado en 9 dígitos.
+    if(largo > 9){  
+      final = final.slice(0,9);    
+     return final;
+    }else{
+     return final;
+    }
+    
+  }
+
+}
